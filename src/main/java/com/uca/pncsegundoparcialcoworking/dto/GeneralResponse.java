fe9c228de7ab702091e3fbf8ec117a235b778455
JonaSeguReymundo <00043823@uca.edu.sv>
2026-05-25 19:08:29 -0600

    feat: adding GeneralResponse file, the structure of the general response

---
+++ src/main/java/com/uca/pncsegundoparcialcoworking/dto/GeneralResponse.java
@@ -0,0 +1,13 @@
+package com.uca.pncsegundoparcialcoworking.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class GeneralResponse {
+    Object data;
+    String message;
+}
---
+++ src/main/java/com/uca/pncsegundoparcialcoworking/dto/request/SpaceRequest.java
@@ -0,0 +1,4 @@
+package com.uca.pncsegundoparcialcoworking.dto.request;
+
+public class SpaceRequest {
+}
